c02f5afec85f9cc5fa9e201ab464575007601e33
Evanzs <evandrofidk@hotmail.com>
2019-11-13 15:37:00 -0300

    Criação da estrutura do grafo

---
+++ reuniao.c
@@ -0,0 +1,35 @@
+/* 
+   Resolução OBI 2010 Reunião
+
+   Feito Por:
+   Evandro Fernandes Barreto
+   João Andreossi
+   Unesp-Bauru Novembro 2019*/
+   
+   
+   
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+main ()
+{
+     int N,M,U,W; 
+     
+     scanf ("%d",&N);
+     scanf ("%d",&M);
+     
+     
+     
+     
+     
+     
+}
+
+
+
+
+
+   
